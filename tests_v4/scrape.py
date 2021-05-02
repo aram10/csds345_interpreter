c42b525003780230d6fea047488dba70a9f6a4f1
Alexander Rambasek <atr51@case.edu>
2021-05-02 13:56:40 -0400

    parser, tests

---
+++ tests_v4/scrape.py
@@ -0,0 +1,11 @@
+from os import close
+from bs4 import BeautifulSoup
+
+f = open("part4tests.html")
+soup = BeautifulSoup(f, 'html.parser')
+
+for i, text in enumerate(soup.find_all('pre')): 
+    with open(f'{i}.txt', 'w+') as test_file:
+        test_file.write(text.string[1::])
+
+f.close()
